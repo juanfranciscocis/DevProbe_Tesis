501773ba0fffa58fc49bd750880d97b0c9e9ec09
Juan Cisneros <jfcisnerosg@icloud.com>
2024-05-27 13:54:25 -0500

    firestore

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, provideFirebaseApp(() => initializeApp({"projectId":"devprobe-89481","appId":"1:405563293900:web:ba12c0bd15401fd708c269","storageBucket":"devprobe-89481.appspot.com","apiKey":"AIzaSyAORx8ZNhFZwo_uR4tPEcmF8pKm4GAqi5A","authDomain":"devprobe-89481.firebaseapp.com","messagingSenderId":"405563293900"})), provideFirestore(() => getFirestore())],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
