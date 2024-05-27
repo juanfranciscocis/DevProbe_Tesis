687aa533f07ad1a8aeca4061de86b1838b66eeee
Juan Cisneros <jfcisnerosg@icloud.com>
2024-05-27 13:06:22 -0500

    INIT

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,10 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'DevProbe',
-  webDir: 'www'
+  webDir: 'www',
+  server: {
+    url: 'http://localhost:8100',
+  }
 };
 
 export default config;
