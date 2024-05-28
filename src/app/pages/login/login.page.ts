83f28bc7933961e7a6b905e1a3d94914bd6a164b
Juan Cisneros <jfcisnerosg@icloud.com>
2024-05-28 14:04:09 -0500

    New Features: 1. Login Page design New Pages: Bugs Corrected:
    To Be Corrected:
    1.Go to Home page on register
    2.Create a new Team on register
    3.Make user LogIn

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +7,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
+  /**
+   * Email input field.
+   * @Input decorator to bind this property with a parent component.
+   */
+  @Input() email:string="";
+
+  /**
+   * Password input field.
+   * @Input decorator to bind this property with a parent component.
+   */
+  @Input() password:string="";
+
+  /**
+   * LoginPage constructor.
+   */
   constructor() { }
 
+  /**
+   * A lifecycle hook that is called after Angular has initialized all data-bound properties of a directive.
+   */
   ngOnInit() {
   }
 
+  /**
+   * Login a user.
+   * If all the fields are filled, it creates a new user object and calls the loginUser method.
+   * If the login is successful, it logs "User logged in successfully!".
+   * If there is an error, it logs "There was an error logging in the user".
+   * If not all the fields are filled, it logs "Please enter your email and password".
+   */
+  loginUser() {
+
+  }
 }
