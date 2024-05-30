15e566e3a8629c186645f13b62958a3062e5fdbb
Juan Cisneros <jfcisnerosg@icloud.com>
2024-05-30 12:13:01 -0500

    New Features: 1.  Logic to create the new product in design New Pages:
    Bugs Corrected:
    
    To Be Corrected:
    1. Add a new page to see a product thats created
    2. Needs ti be sent to firebase
    3. Logic to show products
    4. Logic to see a product detail (of item 1)

---
+++ src/app/interfaces/product.ts
@@ -0,0 +1,6 @@
+export interface Product {
+  productObjective: string;
+  productSteps: string[];
+  productServices: string[];
+  productSLOs: string[];
+}
--- src/app/pages/new-product/new-product.page.ts
+++ src/app/pages/new-product/new-product.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {Product} from "../../interfaces/product";
 
 @Component({
   selector: 'app-new-product',
@@ -7,9 +8,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewProductPage implements OnInit {
 
+  @Input() productObjective:string = '';
+  @Input() productStep:string = '';
+  @Input() productService:string = '';
+  @Input() productSLO:string = '';
+
+  new_product:Product = {
+    productObjective: '',
+    productSteps: [],
+    productServices: [],
+    productSLOs: []
+  }
+
+
+
+
+
   constructor() { }
 
   ngOnInit() {
   }
+
+  async addProductStep() {
+    this.new_product.productSteps.push(this.productStep);
+    this.productStep = '';
+  }
+
+  async addProductService() {
+    this.new_product.productServices.push(this.productService);
+    this.productService = '';
+  }
+
+  async addProductSLO() {
+    this.new_product.productSLOs.push(this.productSLO);
+    this.productSLO = '';
+  }
+
+
+
+
 
 }
