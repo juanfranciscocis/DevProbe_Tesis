5c3be67bc68436ccd9deca30904b16398ea18d02
Juan Cisneros <jfcisnerosg@icloud.com>
2024-08-23 16:34:52 -0500

    New Features: 1. Make a chat session in the Traceroute page New Pages: Bugs Corrected: To Be Corrected: 0. On product delete, delete trace results 1. On product delete, delete flamegraph results

--- src/app/pages/graph-trace/graph-trace.page.ts
+++ src/app/pages/graph-trace/graph-trace.page.ts
@@ -30,6 +30,29 @@ export class GraphTracePage implements OnInit {
   aiModal: boolean = false
   message: string = '';
   vertexAI: VertexAI = inject(VertexAI);
+  model = getGenerativeModel(this.vertexAI, { model: "gemini-1.5-flash" });
+  chat = this.model.startChat({
+    history: [
+      {
+        role: "user",
+        parts: [{text:"Hola, desde ahora en adelante quiero que seas un modelo experto en Software Quality Assurance y analista de datos"}],
+      },
+      {
+        role: "model",
+        parts: [{text:"Soy un modelo experto en Software Quality Assurance, de igual forma tengo un masterado en anlaítica de datos ¿En qué puedo ayudarte?"}],
+      },
+      {
+        role: "user",
+        parts: [{text:"Gracias, te voy a entregar un json con datos en unos minutos, necesito que lo analices y me des un resumen de los datos, este json contiene datos del uso de cpu, imagina que " +
+            "que es un json donde se monitorea al servidor, tu analisis tiene que ser detallado, si encuentras inconsistencias en los datos, por favor mencionalas, en " +
+            "caso de que creas que ocurrio un ataque cibernetico menciona el posible ataque, si crees que hay pocos datos para hacer un verdadero analisis mencionalo "}],
+      },
+      {
+        role: "model",
+        parts: [{text:"Claro, envíame el json y yo me encargo de analizarlo, desde ahora me voy a llamar DevProbeAI, siempre recordare mi nombre (DevProbeAI) ."}]
+      },
+    ],
+  });
   messages:AiMessage[] = []
 
 
@@ -293,26 +316,17 @@ export class GraphTracePage implements OnInit {
     }
   }
 
-  sendMessage() {
+  async sendMessage() {
     if (this.message === '') {
       console.log('Message is empty');
       return;
     }
 
-    this.messages.push({ message: this.message, from: 'User' });
+    this.messages.push({message: this.message, from: 'User'});
 
-    const model = getGenerativeModel(this.vertexAI, { model: "gemini-1.5-flash" });
-    model.generateContent(this.message).then(response => {
-      const answer = response.response.text();
-      const from = 'AI';
-      this.messages.push({ message: answer, from: from });
-
-    }).then(() => {
-      this.message = '';
-      this.scrollToBottom(); // Asegurarte de hacer scroll hacia abajo después de recibir el mensaje
-    });
-
-
+    const result = await this.chat.sendMessage(this.message);
+    this.messages.push({message: result.response.text(), from: 'AI'});
+    this.message = '';
   }
 
 
