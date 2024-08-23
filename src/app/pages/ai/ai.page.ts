c059f9f292d05c22b8e07f670a7e25cc112f1feb
Juan Cisneros <jfcisnerosg@icloud.com>
2024-08-23 15:05:59 -0500

    New Features: 1. Make a chat session! Test New Pages: Bugs Corrected: To Be Corrected: 0. On product delete, delete trace results 1. On product delete, delete flamegraph results

--- src/app/pages/ai/ai.page.ts
+++ src/app/pages/ai/ai.page.ts
@@ -12,17 +12,39 @@ export class AiPage implements OnInit {
   answer: string | undefined;
 
 
+  model = getGenerativeModel(this.vertexAI, {model: "gemini-1.5-flash"});
+  chat = this.model.startChat({
+    history: [
+      {
+        role: "user",
+        parts: [{text:"Hola, desde ahora en adelante quiero que seas un modelo experto en Software Quality Assurance y analista de datos"}],
+      },
+      {
+        role: "model",
+        parts: [{text:"Soy un modelo experto en Software Quality Assurance, de igual forma tengo un masterado en anlaítica de datos ¿En qué puedo ayudarte?"}],
+      },
+      {
+        role: "user",
+        parts: [{text:"Gracias, te voy a entregar un json con datos en unos minutos, necesito que lo analices y me des un resumen de los datos, este json contiene datos del uso de cpu, imagina que " +
+            "que es un json donde se monitorea al servidor, tu analisis tiene que ser detallado, si encuentras inconsistencias en los datos, por favor mencionalas, en " +
+            "caso de que creas que ocurrio un ataque cibernetico menciona el posible ataque, si crees que hay pocos datos para hacer un verdadero analisis mencionalo "}],
+      },
+      {
+        role: "model",
+        parts: [{text:"Claro, envíame el json y yo me encargo de analizarlo, desde ahora me voy a llamar DevProbeAI."}]
+      },
+    ],
+  });
+
+
   constructor() { }
 
   ngOnInit() {
   }
 
-  askQuestionNow() {
-    const model = getGenerativeModel(this.vertexAI, {model: "gemini-1.5-flash"});
-    model.generateContent(this.question!).then(response => {
-      console.log(response.response.text());
-      this.answer = response.response.text();
-    });
+  async askQuestionNow() {
+    const msg = this.question!;
+    const result = await this.chat.sendMessage(msg);
+    this.answer = result.response.text();
   }
-
 }
